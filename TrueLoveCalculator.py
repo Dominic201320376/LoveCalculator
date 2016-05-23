5233dc0170a8d57993305ea7052526b4a0e510f7
Dominic201320376 <dominic.truelen@gmail.com>
2016-05-23 17:59:36 +0800

    Fix

--- TrueLoveCalculator.py
+++ TrueLoveCalculator.py
@@ -1,5 +1,5 @@
 class TrueLoveCalculator:
-	
+
 	def __init__(self, name1, name2):
 		self.name1 = name1
 		self.name2 = name2
@@ -8,14 +8,14 @@ class TrueLoveCalculator:
 		self.count(self.name1)
 		self.count(self.name2)
 
-		self.result()
+		self.output()
 
 	def count(self, string):
 		for char in string:
 			if char in self.trueLove:
 				self.trueLove[char] += 1
 
-	def result(self):
+	def output(self):
 		true = self.trueLove["t"] + self.trueLove["r"] + self.trueLove["u"] + self.trueLove["e"]
 		love = self.trueLove["l"] + self.trueLove["o"] + self.trueLove["v"] + self.trueLove["e"]
 
